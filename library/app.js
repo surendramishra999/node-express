f03209566c538f212b274cc84cf874a1b0f2962d
root <root@surend3145420-2.Nagarro.local>
2019-05-12 16:47:23 +0530

    Pug template added

--- library/app.js
+++ library/app.js
@@ -1,55 +1,65 @@
-const express = require("express");
-const chalk = require("chalk");
-const debug = require("debug")("app");
-const morgan = require("morgan");
-const path = require("path");
-logger = require("./logger");
-var app = express();
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const logger = require('./logger');
 
-const PORT = 3000;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.use(
-  morgan("dev", {
-    skip: function(req, res) {
+  morgan('dev', {
+    skip(req, res) {
       return res.statusCode < 400;
     },
-    stream: process.stderr
-  })
+    stream: process.stderr,
+  }),
 );
 
 app.use(
-  morgan("dev", {
-    skip: function(req, res) {
+  morgan('dev', {
+    skip(req, res) {
       return res.statusCode >= 400;
     },
-    stream: process.stdout
-  })
+    stream: process.stdout,
+  }),
 );
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  "/css",
-  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+  '/css',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "node_modules/jquery/dist/"))
+  '/js',
+  express.static(path.join(__dirname, 'node_modules/jquery/dist/')),
 );
-app.get("/", function(req, res) {
-  logger.debug(chalk.yellow("Debug statement"));
-  logger.info(chalk.blue("Info statement"));
-  res.sendFile(path.join(__dirname, "views/index.html"));
+
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+//app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  logger.debug(chalk.yellow('Debug statement'));
+  logger.info(chalk.blue('Info statement'));
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', {
+    title: 'MY Articles',
+    list: ['Math', 'Science', 'English', 'Hindi'],
+  });
 });
 
-app.use(function(req, res, next) {
-  logger.error(chalk.red("404 page requested"));
-  res.status(404).send("This page does not exist!");
+app.use((req, res) => {
+  logger.error(chalk.red('404 page requested'));
+  res.status(404).send('This page does not exist!');
 });
 
-app.listen(PORT, function() {
-  logger.info(chalk.green("Example app listening on port " + PORT));
+app.listen(PORT, () => {
+  debug('In dev Mode');
+  logger.info(chalk.green(`Hi!  Example app listening on port ${PORT}`));
 });
--- library/logger.js
+++ library/logger.js
@@ -1,23 +1,24 @@
-const { createLogger, format, transports } = require("winston");
+const { createLogger, format, transports } = require('winston');
+
 const { printf, simple, timestamp, colorize, combine } = format;
 
-const level = process.env.LOG_LEVEL || "debug";
+const level = process.env.LOG_LEVEL || 'debug';
 
 const colorizer = colorize();
 
 const logger = createLogger({
-  level: level,
+  level,
   format: combine(
     timestamp(),
     simple(),
     printf(msg =>
       colorizer.colorize(
         msg.level,
-        `${msg.timestamp} - ${msg.level}: ${msg.message}`
-      )
-    )
+        `${msg.timestamp} - ${msg.level}: ${msg.message}`,
+      ),
+    ),
   ),
-  transports: [new transports.Console()]
+  transports: [new transports.Console()],
 });
 
 module.exports = logger;
